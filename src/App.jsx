5c95cf7f5670352ee18273fcbbad2f26adc1c882
Abdullah Baig <baigabdullah174@gmail.com>
2024-05-27 17:12:24 +0530

    fin

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useRef, useState, useCallback } from "react";
+
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import { updateUserPlaces } from "./http.js";
+
+function App() {
+  const selectedPlace = useRef();
+
+  const [userPlaces, setUserPlaces] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleStartRemovePlace(place) {
+    setModalIsOpen(true);
+    selectedPlace.current = place;
+  }
+
+  function handleStopRemovePlace() {
+    setModalIsOpen(false);
+  }
+
+  async function handleSelectPlace(place) {
+    setUserPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((p) => p.id === place.id)) {
+        return prevPickedPlaces;
+      }
+      return [place, ...prevPickedPlaces];
+    });
+
+    try {
+      await updateUserPlaces((prevUserPlaces) => [place, ...prevUserPlaces]);
+    } catch (error) {
+      console.error("Failed to update user places:", error);
+    }
+  }
+
+  const handleRemovePlace = useCallback(async function handleRemovePlace() {
+    setUserPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+    );
+
+    try {
+      await updateUserPlaces((prevUserPlaces) =>
+        prevUserPlaces.filter((place) => place.id !== selectedPlace.current.id)
+      );
+    } catch (error) {
+      console.error("Failed to remove user place:", error);
+    }
+
+    setModalIsOpen(false);
+  }, []);
+
+  return (
+    <>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText="Select the places you would like to visit below."
+          places={userPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+
+        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+      </main>
+    </>
+  );
+}
+
+export default App;
